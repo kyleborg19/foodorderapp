ea8f4c23218bbae3fea28a9d6f757ada4e4292ef
kyleborg19 <kyle.borg10@gmail.com>
2023-10-04 15:53:11 +0200

    Initial Commit

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import { createPortal } from "react-dom";
+import styles from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
